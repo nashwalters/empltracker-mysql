db6440148ce06f86a2ce11267bce353e057f5c22
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-02-19 21:25:46 -0500

    Added function to add employees.

--- tracker.js
+++ tracker.js
@@ -45,7 +45,7 @@ function startApp() {
                     //viewRoles();
                     break;
                 case "Add an employee":
-                    //addEmployee();
+                    addEmp();
                     break;
                 case "Add department":
                     //addDepartment();
@@ -59,6 +59,66 @@ function startApp() {
                 default:
                     break;
             }
+    })
+}
+var validName = (input) => {   
+    //ensure that name isn't empty string or contains numbers                              
+    if ( input === "" || input.match(/\d+/g)!=null) {
+       return "Please enter valid name";
+    }
+     return true;
+}  
+function addEmp() {
+    connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    inquirer
+        .prompt([
+            {
+                name: "firstName",
+                type: "input", 
+                message: "Employee's fist name: ",
+                validate: validName
+            },
+            {
+                name: "lastName",
+                type: "input", 
+                message: "Employee's last name: ",
+                validate: validName
+            },
+            {
+                name: "role", 
+                type: "list",
+                choices: function() {
+                var roleArray = [];
+                for (let i = 0; i < res.length; i++) {
+                    roleArray.push(res[i].title);
+                }
+                return roleArray;
+                },
+                message: "What is this employee's role? "
+            }
+            ]).then(function (data) {
+                let roleId;
+                for (let j = 0; j < res.length; j++) {
+                if (res[j].title == data.role) {
+                    roleId = res[j].id;
+                    console.log(roleId)
+                }                  
+                }  
+                connection.query(
+                "INSERT INTO employees SET ?",
+                {
+                    first_name: data.firstName,
+                    last_name: data.lastName,
+                    role_id: roleId,
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("The employee has been added!");
+                    startApp();
+                }
+                )
+            })
     })
 }
 
